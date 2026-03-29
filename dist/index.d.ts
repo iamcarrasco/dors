af9c18b7b98dd79688dcb48402a755ed04a2120e
Alexandre Carrasco <carrascoamm@gmail.com>
2026-03-29 19:39:30 +0100

    chore: add .gitignore, remove build artifacts and internal docs

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { DORSStatus, DORSReasonCode, MCPIDORSStatusEntry, DORSServiceEntry, DORSStatusQuery, DORSAssertionClaims, IssuanceRecord, DORSVerifierPolicy, DORSResponderConfig, DORSVerificationResult, } from './types.js';
-export { computeCredentialHash, createDORSStatusEntry, createIssuanceRecord, } from './issuer.js';
-export { DORSResponder } from './responder.js';
-export { verifyDORSAssertion, fetchDORSAssertion, } from './verifier.js';
--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export { computeCredentialHash, createDORSStatusEntry, createIssuanceRecord, } from './issuer.js';
-export { DORSResponder } from './responder.js';
-export { verifyDORSAssertion, fetchDORSAssertion, } from './verifier.js';
